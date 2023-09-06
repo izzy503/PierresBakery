1e048cde798e7b4ac8657cc2b24ac3877e88e8c6
ipc <israelpadilla@Israels-MacBook-Air.local>
2023-09-05 19:34:12 -0700

    first commit

--- ShapeTracker.Tests/ModelTests/TriangleTests.cs
+++ ShapeTracker.Tests/ModelTests/TriangleTests.cs
@@ -1,11 +1,33 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ShapeTracker.Models;
+using RockPaperScissors.Models;
 
-namespace ShapeTracker.Tests
+namespace RockPaperScissorsTests
 {
   [TestClass]
-  public class TriangleTests
+  public class RockPaperScissors
+
+  [TestMethod]
+  public void Test_Player1Wins_WithRockAndScissors()
   {
-    // Tests will go here!
+    Assert.AreEqual("Player 1", RockPaperScissors.Play("Rock", "Scissors");
   }
+
+  [TestMethod]
+  public void Test_Player2Wins_WithPaperandRock()
+  {
+    Assert.AreEqual("Player 2", RockPaperScissors.Play("Rock , "Paper));
+  }
+
+  [TestMethod]
+  public void Test_Player2Wins_WithScissorsAndPaper()
+  {
+    Assert.AreEqual("Player 2", RockPaperScissors.Play("Paper", "Scissors"));
+  }
+
+  [TestMethod]
+  public void Test_Draw_withSameChoices()
+
+
+  // Tests will go here!
+}
 }
