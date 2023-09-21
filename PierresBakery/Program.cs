ca2fcc1788a159a48d97b47acdd54ca22511dea6
ipc <israelpadilla@Israels-MacBook-Air.local>
2023-09-20 19:04:02 -0700

    correction

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -11,14 +11,12 @@ namespace PierresBakery
       Console.Write("Enter the number of breads you want to buy: ");
       int breadQuantity = int.Parse(Console.ReadLine());
 
-      Console.Write("Enter the price per bread: ");
-      int breadPrice = int.Parse(Console.ReadLine());
 
-      Pastry newBread = new Pastry(PastryQuantity,);
-      Console.WriteLine($"The total cost for pastry is: {newPastry.CalculateTotalCost()}");
+      bread newBread = new Bread(BreadQuantity);
+      Console.WriteLine($"The total cost for pastry is: {newBread.CalculateTotalCost()}");
 
-      Console.Write("Enter the number of pastries you want to buy: ");
-      int pastryQuantity = int.Parse(Console.ReadLine());
+      Console.Write("Enter the number of bread you want to buy: ");
+      int breadQuantity = int.Parse(Console.ReadLine());
 
       Console.Write("Enter the price per pastry: ");
       int breadPrice = int.Parse(Console.ReadLine());
